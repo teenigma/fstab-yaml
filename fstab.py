6ff3a585f8952988d9deabf79743717a405b4721
Teerapatr K <7356983+teenigma@users.noreply.github.com>
2021-12-18 22:17:54 +0700

    Parse filesystem source

--- fstab.py
+++ fstab.py
@@ -3,7 +3,20 @@
 import sys, yaml
 
 def fstab_generator(argv):
-  pass
+  output_lines = ''
+
+  for mount_source in argv:
+    mount_details = argv[mount_source]
+    mount = {}
+
+    # Mount source
+    mount['source'] = mount_source
+    if mount_details['type'] == 'nfs':
+      mount['source'] += ':' + mount_details['export']
+
+    output_lines += mount['source'] + '\n'
+
+  return(output_lines)
 
 def main(argv):
   ## Default filename
@@ -18,6 +31,7 @@ def main(argv):
 
   if 'fstab' in fstab_load:
     result = fstab_generator(fstab_load['fstab'])
+    print(result)
   else:
     print('fstab key is expected.')
 
