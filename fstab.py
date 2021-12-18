8c446bf8df5c1ee50fae6bdd5d04ffaff65bf43f
Teerapatr K <7356983+teenigma@users.noreply.github.com>
2021-12-18 22:06:00 +0700

    Pass YAML to fstab generator

--- fstab.py
+++ fstab.py
@@ -2,6 +2,9 @@
 
 import sys, yaml
 
+def fstab_generator(argv):
+  pass
+
 def main(argv):
   ## Default filename
   input_file = 'fstab.yaml'
@@ -12,6 +15,11 @@ def main(argv):
       fstab_load = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
       print(exc)
+
+  if 'fstab' in fstab_load:
+    result = fstab_generator(fstab_load['fstab'])
+  else:
+    print('fstab key is expected.')
 
 if __name__ == "__main__":
   main(sys.argv[1:])
