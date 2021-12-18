206afedad2ec54714cd44013bab950bdd424c586
Teerapatr K <7356983+teenigma@users.noreply.github.com>
2021-12-18 22:00:39 +0700

    Load YAML parser, define in/out files, read YAML file, sample of input

--- fstab.py
+++ fstab.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, yaml
 
 def main(argv):
-  pass
+  ## Default filename
+  input_file = 'fstab.yaml'
+  output_file = 'results/fstab.out'
+
+  with open(input_file, "r") as stream:
+    try:
+      fstab_load = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+      print(exc)
 
 if __name__ == "__main__":
   main(sys.argv[1:])
